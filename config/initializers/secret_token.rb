4f7d7992306e908e27543a69fb905a5a52088877
Quantum Mechanics <writewale@netscape.net>
2013-02-01 16:54:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McbrideMasonry::Application.config.secret_token = '9ea7ace7ba798aa1007cd6b8de91a0a041bad03d02797d579eb1036debef5d56ab3f2131017c5ddad1652e93a70dffad2985341328e665a1f7ab61a5d5bec755'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+McbrideMasonry::Application.config.session_store :cookie_store, key: '_mcbride_masonry_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# McbrideMasonry::Application.config.session_store :active_record_store
